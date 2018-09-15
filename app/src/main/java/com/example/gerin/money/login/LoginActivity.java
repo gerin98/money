2a2b84f676a7d9ab29a5b9e5c8f47d12ac954ed5
gerin98 <gerin.amalaraj@mail.utoronto.ca>
2018-09-15 04:19:22 -0400

    added sidebar

--- app/src/main/java/com/example/gerin/money/login/LoginActivity.java
+++ app/src/main/java/com/example/gerin/money/login/LoginActivity.java
@@ -1,12 +1,14 @@
 package com.example.gerin.money.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.crashlytics.android.Crashlytics;
+//import com.crashlytics.android.Crashlytics;
+import com.example.gerin.money.MainActivity;
 import com.example.gerin.money.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -16,10 +18,12 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button crashButton = findViewById(R.id.signin_button);
-        crashButton.setText("Crash!");
+        crashButton.setText("Enter");
         crashButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Crashlytics.getInstance().crash(); // Force a crash
+               Intent nextIntent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(nextIntent);
+                finish();
             }
         });
     }
