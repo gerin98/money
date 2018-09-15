1d8f85c5816747acde1e421b1c5ea73ff2f7f1cd
Sai Vikranth Desu <vikranthdesu@gmail.com>
2018-09-15 03:18:09 -0400

    login plus firebase

---
+++ app/src/main/java/com/example/gerin/money/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.gerin.money.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.gerin.money.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
