1dab88d256f0c75f161215927860a2926c2921f8
Sai Vikranth Desu <vikranthdesu@gmail.com>
2018-09-15 03:30:19 -0400

    crashlytics test

--- app/src/main/java/com/example/gerin/money/login/LoginActivity.java
+++ app/src/main/java/com/example/gerin/money/login/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.gerin.money.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.crashlytics.android.Crashlytics;
 import com.example.gerin.money.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -10,5 +14,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button crashButton = findViewById(R.id.signin_button);
+        crashButton.setText("Crash!");
+        crashButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Crashlytics.getInstance().crash(); // Force a crash
+            }
+        });
     }
 }
