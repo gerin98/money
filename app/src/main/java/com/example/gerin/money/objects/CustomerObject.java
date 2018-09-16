24dcf140a071f63bb4269c555e9bf8a64eeca04c
Tyler DiBattista <tylerdibo@hotmail.com>
2018-09-16 00:18:54 -0400

    Implemented data binding so that the UI is updated as soon as api data is available.

---
+++ app/src/main/java/com/example/gerin/money/objects/CustomerObject.java
@@ -0,0 +1,8 @@
+package com.example.gerin.money.objects;
+
+import java.util.ArrayList;
+
+public class CustomerObject {
+    public ArrayList<String> accountIDs = new ArrayList<>();
+    public String name = "";
+}
