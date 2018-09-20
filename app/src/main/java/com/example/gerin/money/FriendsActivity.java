45c7378842cb06eec3a9c9e1556e50d8122afa84
Sai Vikranth Desu <vikranthdesu@gmail.com>
2018-09-19 22:12:24 -0400

    quick

--- app/src/main/java/com/example/gerin/money/FriendsActivity.java
+++ app/src/main/java/com/example/gerin/money/FriendsActivity.java
@@ -1,25 +1,54 @@
 package com.example.gerin.money;
 
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FriendsActivity extends AppCompatActivity {
 
-    String[] mobileArray = {"Sam (Me)","Jim","Bob"};
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
 
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        String username = "";
+
+        mDatabase.child("users").child(user.getUid()).child("fullname").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String username = dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        String[] mobileArray = { username + "(Me)","Jim","Bob"};
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_list_item, mobileArray);
 
         ListView listView = (ListView) findViewById(R.id.my_listview);
         listView.setAdapter(adapter);
-
-
     }
 }
